cdfaab1835e973e5ebd6d333c31e1d36047d6676
lilin <lilin@itcom888.com>
2019-12-27 14:21:55 +0800

    添加视图模板的热更新

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,40 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { blue } from 'colors';
+import { join } from 'path';
+import { registerPartials } from 'hbs';
+
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  addEngine(app);
+
   const port = 6333;
-  await app.listen(port);
-  console.log(blue(`http server is start at ===> http://localhost:${port}`));
+  await app.listen(port, () => {
+    console.log(blue(`http server is start at ===> http://localhost:${port}`));
+  });
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
+
+function addEngine(app) {
+  // 设置public文件存放文件夹
+  app.useStaticAssets(join(__dirname, '..', 'public'), {
+    prefix: '/public/',
+  });
+  // 设置静态文件存放文件夹
+  app.useStaticAssets(join(__dirname, '..', 'static'), {
+    prefix: '/static/',
+  });
+  // 设置视图文件夹
+  app.setBaseViewsDir(join(__dirname, '..', '/views'));
+  // 设置视图引擎
+  app.setViewEngine('hbs');
+  // 设置视图部件的文件夹
+  registerPartials(join(__dirname, '..', '/views/partials'));
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,32 +1,16 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
+import { Controller, Post, Get, Render } from '@nestjs/common';
 
-@Controller('user')
+@Controller('')
 export class UserController {
-  @Get()
-  public renderLoginPage(): string {
-    return `<div style="color:blue">
-        <form action="/user/login" method="post">
-            <div>
-                <label for="name">Name:</label>
-                <input name="name" type="text" id="name">
-            </div>
-            <div>
-                <label for="mail">E-mail:</label>
-                <input name="email" type="email" id="mail">
-            </div>
-            <div>
-                <label for="msg">description:</label>
-                <textarea name="description" id="description"></textarea>
-            </div>
-            <button type="submit">Send your message</button>
-        </form>
-    </div>`;
+  @Get('login')
+  @Render('login.hbs')
+  public login(): string {
+    return '';
   }
 
-  @Post('login')
-  public login(
-    @Body() loginInfo: { name: string; email: string; description: string },
-  ): string {
-    return `<div>${loginInfo.name} + ${loginInfo.email} + ${loginInfo.description}</div>`;
+  @Get('register')
+  @Render('register.hbs')
+  public register(): string {
+    return '';
   }
 }
